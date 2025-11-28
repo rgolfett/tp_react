02b0ed7803d8505d43e2fedd88c684694a7d5559
Romane Golfetto <rgolfett@z4r4p2.42lyon.fr>
2025-11-28 11:38:05 +0100

    restored

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
 import { Box } from './Box'
-//import { Librairie } from './section/Librairie'
+import { Librairie } from './section/Librairie'
 
 function App() {
 
   return (
     <div> 
       <Box name="hello World"/>
+      <Box name="hello ewdwwe"/>
+
+      <Box name="ewdee World"/>
+
+      <Librairie/>
+
     </div>
   )
 }
--- src/Chat.tsx
+++
@@ -1,8 +0,0 @@
-
-type Message = {
-id: string;
-author: string;
-text: string;
-time: number;
-};
-
